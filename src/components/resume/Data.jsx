dfdc7cf0298328ed02320b9363b6c2cab58a7324
ankitgwari <ankitgwari14@gmail.com>
2024-09-26 01:18:38 +0530

    experience section complete

---
+++ src/components/resume/Data.jsx
@@ -0,0 +1,29 @@
+const Data = [
+  {
+    id: 1,
+    category: "education",
+    icon: "icon-graduation",
+    year: "2021 - 2025",
+    title: "Bachelor of Technology",
+    desc: "Uttaranchal University, Dehradun",
+  },
+  {
+    id: 2,
+    category: "education",
+    icon: "icon-graduation",
+    year: "2020 - 2021",
+    title: "Intemediate",
+    desc: "Army School, Hempur",
+  },
+
+  {
+    id: 3,
+    category: "experience",
+    icon: "icon-briefcase",
+    year: "Oct 2024 - Present",
+    title: "Intern",
+    desc: "Optimus Information",
+  },
+];
+
+export default Data;
